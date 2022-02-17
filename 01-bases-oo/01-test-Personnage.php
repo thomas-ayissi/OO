2d3c924acc4bef45e51fdb4bd2a4aa6d8a97daef
mikhawa <michaeljpitz@gmail.com>
2022-02-17 16:56:37 +0100

    Personnage methodes

--- 01-bases-oo/01-test-Personnage.php
+++ 01-bases-oo/01-test-Personnage.php
@@ -7,8 +7,10 @@ require_once "Personnage.php";
 
 // on va instancier, donc créer un objet, en utilisant le mot clef "new", et on va le mettre dans une variable ($moi)
 
+// instance 1
 $moi = new Personnage();
 
+// instance 2
 $toi = new Personnage();
 
 
@@ -16,6 +18,35 @@ $toi = new Personnage();
 echo '$moi est une instance de Personnage (1er instance) :';
 // affichage de l'objet (instance) // object(Personnage)[1]
 var_dump($moi);
+
 echo '$toi est une instance de Personnage (2eme instance) :';
 // affichage de l'objet (instance) // object(Personnage)[2]
 var_dump($toi);
+
+echo "<h4>Affichage d'attributs publics</h4>";
+echo '<p>echo $moi->nom affiche ' . $moi->nom . '</p>';
+echo '<p>echo $toi->nom affiche ' . $toi->nom . '</p>';
+
+echo "<h4>L'Affichage d'attributs non publics n'est pas possible de la même manière, ni leur modification </h4>";
+//echo '<p>echo $moi->force affiche ' . $moi->force . '</p>';
+// echo '<p>echo $toi->force affiche ' . $toi->force . '</p>';
+echo "Erreur Fatale de type : Fatal error: Uncaught Error: Cannot access private property Personnage::force in D:\CF2m\WEB2021\Qualifiante\PHP\OO\OO\01-bases-oo\01-test-Personnage.php on line 31";
+
+echo "<h4>Modification d'attributs publics</h4>";
+echo "<p>Sont facile à faire (trop !)</p>";
+
+$moi->nom = "Michaël";
+$toi->nom = 425;
+echo '$moi->nom = "Michaël"; va modifier $moi->nom';
+echo '<p>echo $moi->nom affiche ' . $moi->nom . '</p>';
+echo '$toi->nom = 425; va modifier $toi->nom, Mais on ne voulait pas du numérique !';
+echo '<p>echo $toi->nom affiche le numérique ' . $toi->nom . '</p>';
+echo '<h3>Risque de bugs volontaires ou pas, parfois non trouvés avant la production, donc gros danger niveau responsabilité </h3>';
+
+echo "<h3>Les méthodes publiques</h3>";
+echo "<p>Ce sont les actions permises à l'objet en dehors de la classe (fonctions)</p>";
+
+$moi->afficheForce();
+$moi->afficheForce();
+$moi->afficheForce();
+$toi->afficheForce();
--- 01-bases-oo/Personnage.php
+++ 01-bases-oo/Personnage.php
@@ -9,7 +9,33 @@
 
 class Personnage
 {
-    // Attributs
+    // Attributs (ce sont des variables) - property (propriété)
 
-    // Méthodes
+    // privées (innacessible en dehors de la classe)
+    private $force = 50;
+    private $endurance = 100;
+    private $degats;
+
+    // publiques (accessibles et modifiables en dehors de la classe)
+    public $nom = "Anonyme";
+    public $experience;
+
+    // Constantes
+
+    // Méthodes (fonctions précédées d'une visibilité qui permettent de faire agir l'instance de classe)
+
+    // les fonction  publiques peuvent être appelées en dehors de la classe
+    public function afficheForce()
+    {
+        // affichage de texte + $this représente l'instance et affichage d'un attribut privé.
+        echo "La force du personnage est {$this->force}";
+        // on peut appeler la fonction privée ici, car on est dans la classe (de l'instance)
+        $this->forcePlusUn();
+    }
+
+    // fonction privée, ne peut être appelée que depuis la classe !
+    private function forcePlusUn()
+    {
+        $this->force += 1;
+    }
 }
